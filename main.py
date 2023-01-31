ddef8d1f251ac2a3fe32ae3096f20e0a915363a5
BesedinOleg <79640118+MrKykypy3ka@users.noreply.github.com>
2023-01-31 21:51:49 +0900

    API uandex

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import requests
 import json
-from requests.structures import CaseInsensitiveDict
+import shutil
+from PIL import *
 
 
-#resp = requests.post(url, headers=headers, data=data)
-#print(resp.status_code)
-
-
-def main():
+def twoGis():
     link = 'https://routing.api.2gis.com/carrouting/6.0.0/global?key=1967db2b-de7d-46b3-b63a-ab040702a18a'
     headers = json.load(open("headers.json", "r"))
     data = open("data.json", "r").read()
@@ -15,7 +12,12 @@ def main():
     question = requests.post(url=link, headers=headers, data=data).text
     result = json.loads(question)
     print(result)
+def yandex():
+    link = f'https://static-maps.yandex.ru/1.x/?ll={127.53559},{50.26585}&size={650},{450}&spn={0.007},{0.007}&l=map,trf' #50.26585 с.ш. 127.53559 в.д.
+    question = requests.get(url=link, stream=True)
+    with open('img.png', 'wb') as out_file:
+        shutil.copyfileobj(question.raw, out_file)
 
 
 if __name__ == "__main__":
-        main()
+        yandex()
