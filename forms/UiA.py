e8c8b8762af9c3b1f05ce68df68a6ffaaaf92038
BesedinOleg <79640118+MrKykypy3ka@users.noreply.github.com>
2023-02-22 03:28:47 +0900

    add: request to BD

--- forms/UiA.py
+++ forms/UiA.py
@@ -2,8 +2,10 @@ from PyQt5 import uic, QtWidgets, QtCore
 from forms.add_Form import Ui_addForm
 from PyQt5.QtGui import QPixmap
 from API.yandex import get_map
+import sqlite3
 
 Form, Window = uic.loadUiType("forms/add_form.ui")
+
 
 class MouseTracker(QtCore.QObject):
     positionChanged = QtCore.pyqtSignal(QtCore.QPoint)
@@ -42,12 +44,50 @@ class UiA(QtWidgets.QDialog, Form):
         self.uia.addTimeButton.clicked.connect(self.add_time)
         self.uia.imageMap.clicked.connect(self.map_click)
         self.uia.viewButton.clicked.connect(self.load_map)
-
+        self.uia.saveButton.clicked.connect(self.save)
         tracker = MouseTracker(self.uia.imageMap)
         tracker.positionChanged.connect(self.on_positionChanged)
+
         self.new_longitude = self.uia.longitudeEdit.text()
         self.new_latitude = self.uia.latitudeEdit.text()
 
+        self.db = sqlite3.connect('load.sqlite')
+        self.sql = self.db.cursor()
+
+
+    def save(self):
+        print(1)
+        self.save_bad_sections()
+        self.save_road()
+        self.save_crossroad()
+        self.save_time()
+        self.save_date()
+
+    def save_bad_sections(self):
+        s_id = 1
+        print([self.uia.badRoadList.item(i) for i in range(self.uia.badRoadList.count())])
+        for elem in self.uia.badRoadList.items():
+            # sqlite_insert_query = "INSERT INTO badSections (badSection_id, street, longitude, latitude, count_lane, max_speed) VALUES (?, ?, ?, ?, ?, ?);"
+            temp = elem.replace(',', '').replace('(', '').replace(')', '')
+            longitude = elem
+            print(longitude)
+            # latitude =
+            # data = (s_id, "", )
+            # sql.execute(sqlite_insert_query, data)
+            # db.commit()
+            # s_id += 1
+
+    def save_road(self):
+        pass
+
+    def save_crossroad(self):
+        pass
+
+    def save_time(self):
+        pass
+
+    def save_date(self):
+        pass
 
     def map_click(self):
         self.x = -(325 - self.pos_x)
